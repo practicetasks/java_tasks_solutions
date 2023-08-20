de99fe00669eb9f668cd227a2152266230a98dcf
= <=>
2023-08-20 18:17:46 +0600

    lists_and_hashtables_task_3

---
+++ lists_and_hashtables/task_3/ExpensesManager.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class ExpensesManager {
+    ArrayList<Double> expenses;
+
+    ExpensesManager() {
+        expenses = new ArrayList<>();
+    }
+
+    double saveExpense(double moneyBeforeSalary, double expense) {
+        moneyBeforeSalary = moneyBeforeSalary - expense;
+        expenses.add(expense);
+        System.out.println("Значение сохранено! Ваш текущий баланс в тенге: " + moneyBeforeSalary);
+        if (moneyBeforeSalary < 5000) {
+            System.out.println("На вашем счету осталось совсем немного. Стоит начать экономить!");
+        }
+        return moneyBeforeSalary;
+    }
+
+    void printAllExpenses() {
+        for (int i = 0; i < expenses.size(); i++) {
+            System.out.println("Трата № " + (i + 1) + ". Потрачено " + expenses.get(i) + " тенге");
+        }
+    }
+
+    double findMaxExpense() {
+        double maxExpense = 0;
+        for (Double exp : expenses) {
+            if (exp > maxExpense) {
+                maxExpense = exp;
+            }
+        }
+        return maxExpense;
+    }
+
+    public void removeAllExpenses(){
+        expenses.clear();
+        System.out.println("Список трат пуст.");
+    }
+
+    public void removeExpense(double expense) {
+        if (expenses.contains(expense)) {
+            expenses.remove(expense);
+            System.out.println("Трата удалена!");
+        } else {
+            System.out.println("Такой траты нет");
+        }
+    }
+}
